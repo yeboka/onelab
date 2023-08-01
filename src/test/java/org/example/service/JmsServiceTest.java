1d5b2c2a5aac7b563310f681a5fd16ddb1c18d4f
Yeboka <89529214+Yeboka@users.noreply.github.com>
2023-08-01 17:34:56 +0600

    practice 6

--- src/test/java/org/example/service/JmsServiceTest.java
+++ src/test/java/org/example/service/JmsServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.jms.KafkaProducer;
+import org.example.model.dto.CommentNotificationRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -23,10 +24,12 @@ class JmsServiceTest {
     @Test
     void testSendUser() {
         String topic = "user-topic";
-        String name = "John Doe";
+        CommentNotificationRecord notificationRecord =
+                new CommentNotificationRecord(1L,
+                        2,
+                        "commentText");
+        jmsService.sendCommentNotification(topic, notificationRecord);
 
-        jmsService.sendUser(topic, name);
-
-        verify(kafkaProducer, times(1)).sendUser(topic, name);
+        verify(kafkaProducer, times(1)).sendCommentNotification(topic, notificationRecord);
     }
 }
